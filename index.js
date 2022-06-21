6722a78bb06acbcc97886a79204dccbbe71938ae
Devang Amrutiya <devangamrutiya18@gmail.com>
2022-06-21 11:00:21 +0530

    Post
    done

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var route = require('./route');
+
+var app = express();
+// app.use('/api',route)
+    app.listen(process.env.PORT,()=>{
+    app.listen(3000,()=>{
+    console.log("server Started");
+});
+})
