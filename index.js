2e2b921f8565dc9589395dc35ff59cd724b43757
Devang Amrutiya <devangamrutiya18@gmail.com>
2022-06-22 10:31:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var route = require('./route');
 
-var app = express();
-app.use('/api',route)
-    app.listen(process.env.PORT||3000,()=>{
-    console.log("server Started");
-});
+mongoose.connect("mongodb+srv://devang:devang7492@cluster0.hchkg.mongodb.net/Student?retryWrites=true&w=majority").then(()=>{
+    var app = express();
+    app.use(bodyparser.urlencoded({extended:false}))
+    app.use(express.json())
+    app.use('/api',route);
+
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log("Server Started");
+    })
+}).catch((err)=>{
+    console.log(err);
+})
