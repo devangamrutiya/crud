10c5a2e5521b2257af793a32b6fbe5d071e3403c
Devang Amrutiya <devangamrutiya18@gmail.com>
2022-06-22 10:48:02 +0530

    new

--- route.js
+++ route.js
@@ -1,8 +1,13 @@
 var express = require('express');
+const stud = require('./Model/stud');
 var router = express.Router();
+
 
 router.get("/",(req,res)=>{
     res.sendFile("index.html",{root:__dirname});
 })
 
+router.get('/stud',async(req,res)=>{
+    var imovie = await stud.find();
+})
 module.exports = router;
